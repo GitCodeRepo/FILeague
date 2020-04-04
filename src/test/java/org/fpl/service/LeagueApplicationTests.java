de00f23dfc26b6e0d85bd750b18730cab03d2dd3
AO055200 <AO055200@cernet.net>
2020-04-04 13:06:01 +0530

    tests

--- src/test/java/org/fpl/service/LeagueApplicationTests.java
+++ src/test/java/org/fpl/service/LeagueApplicationTests.java
@@ -1,16 +1,11 @@
 package org.fpl.service;
 
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-
 import org.fpl.service.controller.LeagueController;
-import org.fpl.service.dao.TeamDao;
 import org.fpl.service.model.Team;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.stubbing.OngoingStubbing;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -20,14 +15,15 @@ public class LeagueApplicationTests {
 	@Mock
 	LeagueController leagueController;
 
-	@Mock
-	TeamDao teamDao;
-
 	Team team = new Team(101, "MI", 201, "FPL", 301, "INDIA", 1);
-	String teamJson = "{\"teamsId\":101,\"teamsName\":\"INDIA\",\"leagueId\":301,\"leagueName\":\"FPL\",\"countryId\":201,\"countryName\":\"MI\",\"leaguePosition\":1}";
 
 	@Test
 	public void testAddTeam() throws Exception {
 		Mockito.when(leagueController.addLeagueResult(Mockito.any(Team.class))).thenReturn(team);
+	}
+
+	@Test
+	public void testDeleteTeam() throws Exception {
+		Mockito.when(leagueController.deleteLeagueResult(Mockito.anyInt())).thenReturn(team.toString());
 	}
 }
